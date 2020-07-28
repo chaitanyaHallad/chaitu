57229c712d1c8aff82308f7dae1481f9c4e85b38
chaitanyaHallad <68237112+chaitanyaHallad@users.noreply.github.com>
2020-07-28 17:23:12 +0530

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,43 @@
+uname1 = "aman"
+uname2 = "preet"
+uname3 = "singh"
+uname4 = "akash"
+uname5 = "verma"
+uname6 = "deep"
+pas1 = "111"
+pas2 = "222"
+pas3 = "333"
+pas4 = "444"
+pas5 = "555"
+pas6 = "666"
+username = input("please enter your username: ")
+if username == uname1:
+    password = input(f"hey {username} please enter your password ")
+    if password == pas1:
+        print(f"hello {username} you are loged in now ")
+elif username == uname2:
+    password = input(f"hey {username} please enter your password ")
+    if password == pas2:
+        print(f"hello {username} you are loged in now ")
+elif username == uname3:
+    password = input(f"hey {username} please enter your password ")
+    if password == pas3:
+        print(f"hello {username} you are loged in now ")
+elif username == uname4:
+    password = input(f"hey {username} please enter your password ")
+    if password == pas4:
+        print(f"hello {username} you are loged in now ")
+elif username == uname5:
+    password = input(f"hey {username} please enter your password ")
+    if password == pas5:
+        print(f"hello {username} you are loged in now ")
+elif username == uname6:
+    password = input(f"hey {username} please enter your password ")
+    if password == pas6:
+        print(f"hello {username} you are loged in now ")
+else:
+    print("please enter your correct name and password ")
+
+
+
+
